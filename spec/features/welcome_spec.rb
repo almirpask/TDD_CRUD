06e25c5e4a885ec4c382d2a89ee3a9e24e536a81
almirpask <almir.santos@umanni.com.br>
2018-11-10 18:56:27 -0200

    add root_path

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Welcomes', type: :feature do
   scenario 'Show the welcome message' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Welcome')
   end
 end
