1240182d2122ac4ca3a271fdcc0bff09f3b0f869
almirpask <almir.santos@umanni.com.br>
2018-11-10 19:13:14 -0200

    add customers link on welcome page

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,8 +1,13 @@
 require 'rails_helper'
 
-feature 'Welcomes', type: :feature do
+feature 'Welcome', type: :feature do
   scenario 'Show the welcome message' do
     visit(root_path)
     expect(page).to have_content('Welcome')
+  end
+
+  scenario 'Show the Customers link' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Customers')
   end
 end
